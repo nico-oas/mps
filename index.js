12b5c9dc94cf4da7bfa79336bc095c6ce5daaa3f
Nico Höchstädter <nico.hoechstaedter@gmail.com>
2020-11-14 13:58:50 +0100

    Added Modals for Login and Registration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){
-    $("#test-button").click(function(){
+    /*$("#test-button").click(function(){
         alert("The button has been clicked!");
-    });
+    });*/
 });
