6c1e4895b5086241d1f24da35725ecb25cbdfaed
phildree <philipp.dreesens@gmail.com>
2020-11-20 07:55:41 +0100

    created JS-Body for the fake backend

---
+++ backend.js
@@ -0,0 +1,36 @@
+// init function to prepare all cookies/variables
+function backend_init() {
+
+}
+
+// function that handles the registration through cookies (faking)
+function registration() {
+
+}
+
+// function that handles the login through cookies (faking)
+function login() {
+
+}
+
+// function that handles the logout through cookies (faking)
+function logout() {
+
+}
+
+
+
+/*
+ITEM HANDLING
+---------------------------------
+*/
+
+// function that adds one item to the list of item the user has put in (stored clientsided using cookies)
+function add_item() {
+
+}
+
+// function that retrieves all items that have been submited by the user (stored clientsided using cookies)
+function retrieve_items() {
+
+}
