79112dce0620312539bdc85dd12085654d1bd87a
phildree <philipp.dreesens@gmail.com>
2020-11-20 08:09:59 +0100

    a

--- backend.js
+++ backend.js
@@ -4,12 +4,17 @@ function backend_init() {
 }
 
 // function that handles the registration through cookies (faking)
-function registration() {
+function registration(username, mail, birthdate, region, password, real_name, gender) {
 
 }
 
 // function that handles the login through cookies (faking)
-function login() {
+function login(login_ID, password) {
+
+}
+
+// function that checks whether the user is already logged in
+function check_login() {
 
 }
 
@@ -26,7 +31,7 @@ ITEM HANDLING
 */
 
 // function that adds one item to the list of item the user has put in (stored clientsided using cookies)
-function add_item() {
+function add_item(item) {
 
 }
 
