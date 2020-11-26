7366f6d6027a7d9c4983da9f983935bbfb53f503
Malik Karagöz <malikkaragoz@Maliks-MacBook-Pro.local>
2020-11-26 17:21:36 +0100

    implemented countdown

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ window.addEventListener("load", function(){
     /*$("#test-button").click(function(){
         alert("The button has been clicked!");
     });*/
+
+                                                       
+
+        var countdown = new Date(new Date().getFullYear(), 07, 22);
+        $('#clock').countdown(countdown, {elapse: true})
+        .on('update.countdown', function(event) {
+          var $this = $(this);
+          var clock = '<div class="d-flex flex-wrap justify-content-center pt-4" > <span class="h1 font-weight-bold">%D</span> Day%!d' +
+          '<span class="h1 font-weight-bold">%H</span> Hr' +
+          '<span class="h1 font-weight-bold">%M</span> Min' +
+          '<span class="h1 font-weight-bold">%S</span> Sec </div>';  
+          if (!event.elapsed) {
+            $this.html(event.strftime('<p class="headline" >time left until Earth Overshootday:</p>' + clock));
+          } else {
+            $this.html(event.strftime( '<p class="headline" >time passed since Earth Overshootday:</p>' + clock));
+          }
+        });   
+           
+         
+       
+      
 });
